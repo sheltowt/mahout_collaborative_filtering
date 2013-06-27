1f604a3167fcd7a1339c8f0dadf5c8125b7b7953
Bill Shelton <williamshelton@ubuntu.(none)>
2013-06-26 17:14:56 -0700

    Ran recommendation engine using a slopeone recommender, and reduced evaluation to .748

---
+++ tdunning-MiA-5b8956f/src/main/java/mia/recommender/ch02/SlopeOneRecommender.java
@@ -0,0 +1,43 @@
+package mia.recommender.ch02;
+
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.eval.RecommenderBuilder;
+import org.apache.mahout.cf.taste.eval.RecommenderEvaluator;
+import org.apache.mahout.cf.taste.impl.eval.AverageAbsoluteDifferenceRecommenderEvaluator;
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.recommender.slopeone.SlopeOneRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.Recommender;
+import org.apache.mahout.cf.taste.impl.recommender.slopeone.SlopeOneRecommender;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
+import org.apache.mahout.common.RandomUtils;
+
+import java.io.File;
+
+class EvaluatorIntroSlope {
+
+    private EvaluatorIntroSlope() {
+    }
+
+    public static void main(String[] args) throws Exception {
+        RandomUtils.useTestSeed();
+        DataModel model = new FileDataModel(new File("/home/williamshelton/Desktop/ml-100k/ua.base"));
+
+        RecommenderEvaluator evaluator =
+                new AverageAbsoluteDifferenceRecommenderEvaluator();
+        // Build the same recommender for testing that we did last time:
+        RecommenderBuilder recommenderBuilder = new RecommenderBuilder() {
+            @Override
+            public Recommender buildRecommender(DataModel model) throws TasteException {
+                return new SlopeOneRecommender(model);
+            }
+        };
+        // Use 70% of the data to train; test using the other 30%.
+        double score = evaluator.evaluate(recommenderBuilder, null, model, 0.7, 1.0);
+        System.out.println(score);
+    }
+}
