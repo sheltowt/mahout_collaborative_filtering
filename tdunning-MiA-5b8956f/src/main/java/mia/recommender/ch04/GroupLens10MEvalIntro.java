deaa3ea572b25935301b86f1a4e317554c4eaad2
Bill Shelton <sheltowt@gmail.com>
2013-07-09 13:46:39 -0700

    TanimotoCoefficient about as effective as Euclidian

--- tdunning-MiA-5b8956f/src/main/java/mia/recommender/ch04/GroupLens10MEvalIntro.java
+++ tdunning-MiA-5b8956f/src/main/java/mia/recommender/ch04/GroupLens10MEvalIntro.java
@@ -8,10 +8,7 @@ import org.apache.mahout.cf.taste.impl.eval.AverageAbsoluteDifferenceRecommender
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
 import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
 import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
-import org.apache.mahout.cf.taste.impl.similarity.CachingUserSimilarity;
-import org.apache.mahout.cf.taste.impl.similarity.EuclideanDistanceSimilarity;
-import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
-import org.apache.mahout.cf.taste.impl.similarity.SpearmanCorrelationSimilarity;
+import org.apache.mahout.cf.taste.impl.similarity.*;
 import org.apache.mahout.cf.taste.model.DataModel;
 import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
 import org.apache.mahout.cf.taste.recommender.Recommender;
@@ -33,7 +30,7 @@ final class GroupLens10MEvalIntro {
       @Override
       public Recommender buildRecommender(DataModel model) throws TasteException {
         UserSimilarity similarity = new CachingUserSimilarity(
-                new SpearmanCorrelationSimilarity(model), model);
+                new TanimotoCoefficientSimilarity(model), model);
         UserNeighborhood neighborhood =
           new NearestNUserNeighborhood(100, similarity, model);
         return new GenericUserBasedRecommender(model, neighborhood, similarity);
